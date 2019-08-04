6e860918fdb071738683f9b07fd58c14ae7eab96
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 22:46:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,9 +2,11 @@ function forLoop(arr){
  
   for (i = 0; i < 25; i++){
     if(i !== 1){
-      arr.push()
+      arr.push(`I am ${i} strange loops.`)
     }
-    
+    else {
+      arr.push
+    }
     
 }
 
