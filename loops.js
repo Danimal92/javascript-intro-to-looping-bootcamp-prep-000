4740eeb4b8e24a08ad656d8e7308b289172bc355
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 22:44:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,14 +1,14 @@
 function forLoop(arr){
-  
+  var newArr = []
   for (i = 0; i < 25; i++)
     if (i === 1){
-      arr = ['I am 1 strange loop.'];
+      newArr = ['I am 1 strange loop.'];
     }
     else {
-      arr = [`I am ${i} strange loops.`];
+      newArr = [`I am ${i} strange loops.`];
     }
     
-    return arr
+    return newArr
 }
 
 function whileLoop(number){
