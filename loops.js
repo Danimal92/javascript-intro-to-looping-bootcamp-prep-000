1c21f71590464731eb5db5fd182c8d8fd6a4e22f
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 22:41:54 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,39 @@
+function forLoop(arr){
+  var len = arr.length
+  for (i = 0; i < len; i++)
+    if (i === 1){
+      arr = ['I am 1 strange loop.'];
+    }
+    else {
+      arr = [`I am ${i} strange loops.`];
+    }
+    
+    return arr
+}
+
+function whileLoop(number){
+  counter = number
+  while (counter > 0){
+    console.log(counter)
+    counter--
+  }  
+  
+  return 'done'
+}
+
+
+
+function incrementVariable() {
+  var i = 0;
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(integer){
+  
+  do {
+    console.log('I run once regardless.')
+  }
+  
+  while(incrementVariable() < integer  );
+}
